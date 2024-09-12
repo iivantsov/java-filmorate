fc98876078ab63b5405e2f3bedaee8da05cbe2b3
iivantsov <iiv.88@yandex.ru>
2024-09-12 12:35:31 +0300

    Sprint-12 Added H2 database support

---
+++ src/main/resources/data.sql
@@ -0,0 +1,18 @@
+INSERT INTO mpa_ratings (mpa_name)
+VALUES ('G'),
+       ('PG'),
+       ('PG-13'),
+       ('R'),
+       ('NC-17');
+
+INSERT INTO genres (genre_name)
+VALUES ('Комедия'),
+       ('Драма'),
+       ('Мультфильм'),
+       ('Триллер'),
+       ('Документальный'),
+       ('Боевик');
+
+INSERT INTO friendship_status (status_name)
+VALUES ('UNAPPROVED'),
+       ('APPROVED');
